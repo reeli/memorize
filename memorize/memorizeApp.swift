f0025a7e996dd7d4b33483c8135cbcdea77eaf8b
reeli <reeli.null@gmail.com>
2021-03-09 15:16:02 +0800

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by rrli  on 2021/3/9.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
