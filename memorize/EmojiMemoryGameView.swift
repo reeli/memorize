a711bb78f8ec426638a287733eff44f2735f8491
reeli <reeli.null@gmail.com>
2021-03-12 17:36:38 +0800

    GeometryReader

--- memorize/EmojiMemoryGameView.swift
+++ memorize/EmojiMemoryGameView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-   @ObservedObject var viewModel: EmojiMemoryGame
+    @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
         HStack {
@@ -37,14 +37,26 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3).foregroundColor(Color.orange)
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+        GeometryReader { geometry in
+            ZStack {
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth).foregroundColor(Color.orange)
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    Text(card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill()
+                }
             }
+            .font(Font.system(size: fontSize(for: geometry.size)))
         }
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius:CGFloat = 10.0
+    let edgeLineWidth:CGFloat = 3
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+         min(size.width, size.height)*0.75
     }
 }
